a7509e03eac55a28aaa63986598bb5d81925444a
r-mattasa <rubini.cbk@gmail.com>
2024-05-14 11:28:12 +0300

    Add project files.

---
+++ Migrations/20240507095547_ModifiedImageNameEntity.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuizAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModifiedImageNameEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageName",
+                table: "Questions",
+                type: "nvarchar(50)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageName",
+                table: "Questions",
+                type: "nvarchar(50)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldNullable: true);
+        }
+    }
+}
